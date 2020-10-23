2431cbf095adcd3c87e223bdb0c7c502a2e0df89
Stanley Wang <stanleywxc@yahoo.com>
2020-10-23 13:38:06 -0500

    fixing typos

--- deployment/cdk/deployments/rest_api_stack.py
+++ deployment/cdk/deployments/rest_api_stack.py
@@ -14,25 +14,25 @@ class EcsRestAPICdkStack(core.Stack):
 
         # Create the ECR Repository
         ecr_repository = ecr.Repository(self,
-                                        "reatapi-repository",
-                                        repository_name="reatapi-repository")
+                                        "restapi-repository",
+                                        repository_name="restapi-repository")
 
         # Create the ECS Cluster (and VPC)
         vpc = ec2.Vpc(self,
-                      "reatapi-vpc",
+                      "restapi-vpc",
                       max_azs=3)
 
         # Create ECS Cluster
         cluster = ecs.Cluster(self,
-                              "reatapi-cluster",
-                              cluster_name="reatapi-cluster",
+                              "restapi-cluster",
+                              cluster_name="restapi-cluster",
                               vpc=vpc)
 
         # Create the ECS Task Definition with placeholder container (and named Task Execution IAM Role)
         execution_role = iam.Role(self,
-                                  "reatapi-execution-role",
+                                  "restapi-execution-role",
                                   assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-                                  role_name="reatapi-execution-role")
+                                  role_name="restapi-execution-role")
 
         execution_role.add_to_policy(iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
@@ -49,32 +49,26 @@ class EcsRestAPICdkStack(core.Stack):
 
         # Create the task-definition
         task_definition = ecs.FargateTaskDefinition(self,
-                                                    "reatapi-task-definition",
+                                                    "restapi-task-definition",
                                                     execution_role=execution_role,
-                                                    family="reatapi-task-definition")
-
-        """
-        container = task_definition.add_container(
-            "reatapi-container",
-            image=ecs.ContainerImage.from_registry("rest-api")
-        )
-        """
+                                                    family="restapi-task-definition")
 
         container = task_definition.add_container(
-            "ecs-devops-sandbox",
+            id="ecs-devops-sandbox",
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
         )
 
         # Add Port mapping to container
         container.add_port_mappings(ecs.PortMapping(container_port=9090, protocol=ecs.Protocol.TCP))
+        container.add_port_mappings(ecs.PortMapping(container_port=80, protocol=ecs.Protocol.TCP))
 
         """
         # Create the ECS Service
         service = ecs.FargateService(self,
-                                     "reatapi-service",
+                                     "restapi-service",
                                      cluster=cluster,
                                      task_definition=task_definition,
-                                     service_name="reatapi-service")
+                                     service_name="restapi-service")
         """
         
         # Create NetworkLoadBalancer first
@@ -96,26 +90,18 @@ class EcsRestAPICdkStack(core.Stack):
 
         # Create the service
         service = ecs_patterns.NetworkLoadBalancedFargateService(self,
-                                        "reatapi-service",
+                                        "restapi-service",
                                         cpu=256,
                                         memory_limit_mib=512,
                                         task_definition=task_definition,
                                         cluster=cluster,
                                         desired_count=2,
-                                        service_name="reatapi-service",
+                                        service_name="restapi-service",
                                         load_balancer=loadbalancer)
 
         # Create a HealthCheck
         health = elbv2.HealthCheck(enabled=True, path="/health/check", interval=5)
 
+        # Add the target group created automatically when creating the service to listener
         listener.add_target_groups("restapi-ecs-lb-target-grp", service.target_group)
-
-        """
-        # Add add the service to target and then to NetworkListener
-        listener.add_targets(id="restapi-ecs-lb-target",
-                            port=9090,
-                            target_group_name="restapi-ecs-lb-target-grp",
-                            targets=[service],
-                            health_check=health)
-        """
 
